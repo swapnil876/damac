034addf2643e3167a26e5e230f9b9f32aa01730f
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-02-02 15:37:15 +0530

    commit

--- graphql/company_announcements.js
+++ graphql/company_announcements.js
@@ -1,53 +1,16 @@
 import { gql } from '@apollo/client';
 
 const COMPANY_ANNOUNCEMENTS = gql`
-  query {
-    nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["career"]}]}) {
-      entities {
-        ... on NodeCareer{
-            nid
-            title
-             fieldHeaderImageVideoMobile{
-              targetId
-              description
-              entity{
-                fid
-                filename
-                url
-              }
-            }
-          fieldHeaderImageVideoDesktop{
-            targetId
-            description
-            entity{
-              fid
-              filename
-              url
-            }
-          }
-          fieldMutlipleIcons{
-            entity{
-              ... on ParagraphMultipleIcons{
-                fieldIconImage{
-                  url
-                }
-                fieldIconText
-              }
-            }
-          }
-          fieldVideoUrl{
-            title
-            url {
-              path
-            }
-          }
-          fieldMetaKeywordsCareer
-          fieldMetaTitleCareer
-          fieldMetaDescriptionCareer
-          fieldCanonicalUrlCareer
-        }
+   query {
+      nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["company_announcements"]}]}) {
+         entities {
+         ... on NodeCompanyAnnouncements{
+               nid
+               fieldTitleCa
+            
+         }
+         }
       }
-    }
-  }`;
+   }`;
 
 export { COMPANY_ANNOUNCEMENTS };
---
+++ graphql/contactus.js
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client';
+
+const CONTACTUS = gql`
+query {
+   nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["contact_us"]}]}) {
+      entities {
+      ... on NodeContactUs{
+            nid
+            fieldAddresses{
+            entity{
+               ... on ParagraphMultipleAddressesContactUs{
+               fieldAddress{
+                 value
+               }
+               fieldNumberC{
+                 value
+               }
+               fieldTitleC{
+                 value
+               }
+             }
+           }
+           }
+         
+      }
+      }
+   }
+}`;
+
+export { CONTACTUS };
