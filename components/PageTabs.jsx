e783b4ee9a23267c5ab5a47852b98e45567a5adb
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-02-08 14:15:33 +0530

    pages

--- components/PageTabs.jsx
+++ components/PageTabs.jsx
@@ -5,18 +5,34 @@ import { useMediaQuery } from 'react-responsive'
 
 import Link from 'next/link'
 import styles from '../styles/components/PageTabs.module.css';
-
+import { isMobile } from 'react-device-detect';
 
 // import styles from '../styles/.module.css'
 
+
+// importing React Select
+import Select from "react-dropdown-select";
+
 export default function PageTabs( { tabLinks } ) {
 
+  const optionsInHere = tabLinks.map( (item) => {
+      return { 
+        value: item.url, 
+        label: item.label
+      }
+   })
+
+  const options = [
+    ...optionsInHere
+  ];
 
   return (
 
     <>
         <div className={ styles['pagetabs'] }>
-          {
+        {
+          !isMobile ?
+         
             tabLinks.map( ( link, index ) => (
 
               <Link href={ link.url } key={ index }>
@@ -26,8 +42,17 @@ export default function PageTabs( { tabLinks } ) {
               </Link>
 
             ) )
-          }
+          
+          :
+          <div>
+             <Select className="page_tabs_for_mobile" name=""
+                   value={options.value}
+                   options={options}
+                   placeholder="Meet Our Board Members"/> 
+          </div>
+        }
         </div>
+ 
     </>
     
   )
