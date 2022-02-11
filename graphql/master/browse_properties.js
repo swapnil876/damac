16d95b04e5dce6971bf1206c45e7010d7b2e87bd
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-02-11 17:32:28 +0530

    graphql

--- graphql/master/browse_properties.js
+++ graphql/master/browse_properties.js
@@ -2,22 +2,22 @@ import { gql } from '@apollo/client';
 
 const BROWSE_PROPERTIES = gql`
 query {
-  nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["browse_properties"]}]}) {
-    entities {
-      ... on NodeBrowseProperty{
-         nid,
-         fieldPageTitleBp
-        fieldStaticHeading
-        fieldStaticCol1{
-          value
-        }
-        fieldStaticCol2{
-          value
-        }
-        
-      }
-    }
-  }
-}`;
+   nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["browse_property"]}]}) {
+     entities {
+       ... on NodeBrowseProperty{
+          nid
+          fieldPageTitleBp
+         fieldStaticHeading
+         fieldStaticCol1{
+           value
+         }
+         fieldStaticCol2{
+           value
+         }
+         
+       }
+     }
+   }
+ }`;
 
 export { BROWSE_PROPERTIES };
