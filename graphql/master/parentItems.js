af34023d226021d48aab8db8d94de9bca4d7a3e3
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-02-12 15:12:50 +0530

    Update parentItems.js

--- graphql/master/parentItems.js
+++ graphql/master/parentItems.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-const COUNTRY = gql`
+const PARENTMENUITEMS = gql`
   query{
   taxonomyTermQuery (limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "vid", value: ["parent_menu_items"]}]}) {
     entities{
@@ -12,4 +12,4 @@ const COUNTRY = gql`
   }
 }`;
 
-export { COUNTRY };
+export { PARENTMENUITEMS };
