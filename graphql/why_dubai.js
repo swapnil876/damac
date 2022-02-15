f19f2822ac6969b2b4c4f5fc485432c200bf0971
Akash Mehta <akash@techsperia.com>
2022-02-15 19:24:55 +0530

    why dubai

--- graphql/why_dubai.js
+++ graphql/why_dubai.js
@@ -2,131 +2,99 @@ import { gql } from '@apollo/client';
 
 const WHY_DUBAI = gql`
   query {
-  nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["community"]}]}) {
+    nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["why_dubai"]}]}) {
     entities {
-      ... on NodeCommunity{
-          nid
-          title
-          fieldTagline
-          fieldCity {
-            entity{tid,name}
-          }
-          fieldCountry {
-            entity{tid,name}
-          }
-          fieldImageDesktop {alt,url}
-          fieldImageMobile {alt,url}
-          fieldGalleryDesktop {alt,url}
-          fieldGalleryMobile {alt,url}
-          fieldBrochure {
-            entity{
-              fid
-              filename
+      ... on NodeWhyDubai{
+      nid
+      fieldCanonicalUrlWhydubai
+      fieldMetaTitleWhydubai
+      fieldMetaKeywordsWhydubai
+      fieldMetaDescriptionWhydubai
+      fieldCanonicalUrlWhydubai
+      fieldMainImageDesktopd {
+        targetId
+        url
+      }
+      fieldMainImageMobile {
+        targetId
+        url
+      }
+      fieldHeaderw2
+      fieldDescription
+      fieldMultipleCounters{
+        entity{
+          ... on ParagraphMultipleCountersDubai{
+            id
+            fieldText
+            fieldIcon {
+              targetId
               url
             }
           }
-          fieldBrandIcons{
-            entity{
-              ... on ParagraphMultipleIcons{
-                fieldIconText
-                fieldIconImage{
-                  url
-                }
-              }
-              
-            }
-          }
-          fieldPropertyTypec {
-            entity{
-              tid
-              name
-            }
-          }
-          fieldTitle2
-          fieldDescriptionc2 {
-            value
-          }
-          fieldPropertyTypec {
-            entity{
-              tid
-              name
-            }
-          }
-          fieldStartingFromPrice
-          fieldArea
-          fieldBedRooms
-          fieldLocality
-          fieldStatus
-        
-          fieldCol1ImageDesktopc{alt, url}
-          fieldCol1ImageMobileC{alt, url}
-          fieldCol2Row1Col1ImageDeskc{alt, url}
-          fieldCol2Row1Col1ImageMobic{alt, url}
-          fieldCol2Row1Col2ImageDeskc{alt, url}
-          fieldCol2Row1Col2ImageMobic{alt, url}
-          fieldCol2Row2ImageDesktopc{alt, url}
-          fieldCol2Row2ImageMobilec{alt, url}
-          fieldCol1Textc
-        
-          fieldAmenities{
-            entity{
-              ... on ParagraphMultipleAmenities{
-                id
-                fieldValueAmi
-                fieldIcona{alt, url}
-                fieldTextAmi
-              }
-            }
-          }
-          fieldDescriptionc4
-          fieldGalleryMobileC4{alt, url}
-        
-        fieldCol1ImageDesktopC5{alt, url}
-        fieldCol1ImageMobileC5{alt, url}
-        fieldCol2Row1Col1ImageDesc5{alt, url}
-        fieldCol2Row1Col1ImageMobc5{alt, url}
-        fieldCol2Row1Col2ImageDeskc{alt, url}
-        fieldCol2Row1Col2ImageMobc5{alt, url}
-        fieldCol2Row2ImageDesktopC5{alt, url}
-        fieldCol2Row2ImageMobileC5{alt, url}
-        fieldCol1Textc5
-        
-        fieldAboutText{value}
-        fieldMasterplan{entity{fid,filename,url}}
-        fieldVideo{title, url {
-          path
-          routed
-        }}
-        fieldMultipleLocators{entity{
-          ... on ParagraphMultipleLocators{
+        } 
+      }
+      fieldHeaderw2
+      fieldSec2Col1Text
+      fieldSec2Col2Text
+       fieldSec3Col1Text
+      fieldHeaderw3
+      fieldCol1Text3
+      fieldSec3Col2Text
+      fieldMutlipleAreas {
+        entity{
+          ... on ParagraphMutlipleAreasDubai{
             id
-            fieldIconm{alt, url}
-            fieldValuec6
-            fieldTextc6
-          }
-        }}
-        fieldMultipleFaqs{
-         entity{
-            ... on ParagraphMultipleFaqs{
-              fieldAnswer
-              fieldQuestion
-            }
+            fieldTitle
+            fieldTextw
           }
         }
-        fieldLongitude
-        fieldLatitude
-        fieldMetaTitleComm
-        fieldMetaDescriptionComm
-        fieldMetaKeywordsComm
-        fieldCanonicalUrlComm
-        fieldSection4Heading
-        fieldText
-        fieldHeading
-        fieldWhatsappNumber
-        fieldEmail
+      }
+      fieldInvestorText
+      fieldMultipleStories {
+        entity{
+          ... on ParagraphMultipleStories{
+            id
+            fieldTextw4
+          }
+        }
+      }
+      fieldOpportunityText
+      fieldMainImageDesktopd {
+        targetId
+        url
+      }
+      fieldMainImageMobile {
+        targetId
+        url
+      }
+      fieldOl2Row1Col1ImageDeskto {
+        targetId
+        url
+      }
+      fieldOl2Row1Col1ImageMobile {
+        targetId
+        url
+      }
+      fieldOl2Row1Col2ImageDeskto {
+        targetId
+        url
+      }
+      fieldCol2Row1Col2ImageMobil {
+        targetId
+        url
+      }
+      fieldCol2Row2ImageDesktop {
+        targetId
+        url
+      }
+      fieldCol2Row2ImageMobile {
+        targetId
+        url
+      }
+      fieldCol1Text5
       }
     }
-  }
-}`;
+    }
+  }`;
 
 export { WHY_DUBAI };
