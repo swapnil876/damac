7e6cea1181273248b2f0b97eea08b9056f570a96
Enpointe <hi@enpointe.in>
2022-02-16 00:09:32 +0530

    update r

---
+++ graphql/inthenewslisting.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+const INTHENEWSLISTING = gql`
+query {
+  nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["in_the_news"]}]}) {
+    entities {
+      ... on NodeInTheNewsListing{
+         nid
+        fieldPageTitleIn
+      }
+    }
+  }
+}`;
+
+export { INTHENEWSLISTING };
