e8bc2aaa57015fd4ef78e855974525cd7282082c
Akash Mehta <akash@techsperia.com>
2022-02-15 11:29:09 +0530

    small change

--- pages/enquire-sales.js
+++ pages/enquire-sales.js
@@ -52,38 +52,38 @@ function ContactUs({nav, othernav}) {
   });
 
   
-   // Use this for novigation
-   const  data2  = await client.query({ query: NAVIGATION });
-   const  data1  = await client.query({ query: PARENTMENUITEMS });
-   let nav = [];
-   let othernav = [];
-   if(typeof data2 != 'undefined' &&  typeof data1 != 'undefined'){
-     let submenu = data2.data.nodeQuery.entities[0];
-     let menu = data1.data.taxonomyTermQuery.entities;
-     console.log('----*-*-*-*-*-*--**------------*-*-*-*-*-*-',data2.data.nodeQuery.entities[0].fieldMultipleMenuItems);
-     // console.log('----*-*-*-*-*-*--*',data1.data.taxonomyTermQuery.entities);
-     menu.map((m,i)=>{
-       othernav = [];
-       let des = m.description==null?'': m.description.value
-       nav.push({name:m.name,tid:m.tid,submenu:[],link:des});
-       if((i+1)==menu.length){
-         submenu.fieldMultipleMenuItems.map((k,l)=>{
-           if(k.entity.fieldMenuType!=null){
-             nav.filter((o,h)=>{
-               if(k.entity.fieldMenuType.entity.tid == o.tid){
-                 o.submenu.push({label:k.entity.fieldMenuNam,url:k.entity.fieldLink});
-               }
-             });
-           }
-           else{
-             othernav.push({label:k.entity.fieldMenuNam,url:k.entity.fieldLink})
-           }
-         })
-       }
-     });
-    
-   }
-     // end
+  // Use this for novigation
+  const  data2  = await client.query({ query: NAVIGATION });
+  const  data1  = await client.query({ query: PARENTMENUITEMS });
+  let nav = [];
+  let othernav = [];
+  if(typeof data2 != 'undefined' &&  typeof data1 != 'undefined'){
+    let submenu = data2.data.nodeQuery.entities[0];
+    let menu = data1.data.taxonomyTermQuery.entities;
+    console.log('----*-*-*-*-*-*--**------------*-*-*-*-*-*-',data2.data.nodeQuery.entities[0].fieldMultipleMenuItems);
+    // console.log('----*-*-*-*-*-*--*',data1.data.taxonomyTermQuery.entities);
+    menu.map((m,i)=>{
+      othernav = [];
+      let des = m.description==null?'': m.description.value
+      nav.push({name:m.name,tid:m.tid,submenu:[],link:des});
+      if((i+1)==menu.length){
+        submenu.fieldMultipleMenuItems.map((k,l)=>{
+          if(k.entity.fieldMenuType!=null){
+            nav.filter((o,h)=>{
+              if(k.entity.fieldMenuType.entity.tid == o.tid){
+                o.submenu.push({label:k.entity.fieldMenuNam,url:k.entity.fieldLink});
+              }
+            });
+          }
+          else{
+            othernav.push({label:k.entity.fieldMenuNam,url:k.entity.fieldLink})
+          }
+        })
+      }
+    });
+   
+  }
+    // end
  
    return {
      props: {
