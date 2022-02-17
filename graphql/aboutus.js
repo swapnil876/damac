2db0131dcabe20400d7fd1bde4784c1c1f329742
swapnil876 <43343687+swapnil876@users.noreply.github.com>
2022-02-17 10:52:22 +0530

    graphql about ud

--- graphql/aboutus.js
+++ graphql/aboutus.js
@@ -1,35 +1,6 @@
 import { gql } from '@apollo/client';
 
 const ABOUT_US = gql`
-# Welcome to GraphiQL
-#
-# GraphiQL is an in-browser tool for writing, validating, and
-# testing GraphQL queries.
-#
-# Type queries into this side of the screen, and you will see intelligent
-# typeaheads aware of the current GraphQL type schema and live syntax and
-# validation errors highlighted within the text.
-#
-# GraphQL queries typically start with a "{" character. Lines that starts
-# with a # are ignored.
-#
-# An example GraphQL query might look like:
-#
-#     {
-#       field(arg: "value") {
-#         subField
-#       }
-#     }
-#
-# Keyboard shortcuts:
-#
-#  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
-#
-#       Run Query:  Ctrl-Enter (or press the play button above)
-#
-#   Auto Complete:  Ctrl-Space (or just start typing)
-#
-
 query {
   nodeQuery(limit: 10, offset: 0, filter: {conditions: [{operator: EQUAL, field: "type", value: ["about_us"]}]}) {
     entities {
